300dd4ff2c5fd32c081fe411c034e8e0fe0844bd
grantmiller <grant@look.io>
2013-09-10 16:51:34 -0700

    Update staging-tag-server.js

--- js/staging-tag-server.js
+++ js/staging-tag-server.js
@@ -1,5 +1,3 @@
-<script type="text/javascript">
   /* LP Mobile JS include */
   (function(){var a=_LP_CFG_.lpjsid="lpjs-"+(new Date).getTime(),b=document.createElement("script"),s=document.getElementsByTagName("script")[0];b.id=a;b.type="text/javascript";b.async=true;b.src="https://tag.staging.look.io/lp_lib/liveperson-mobile.js";s.parentNode.insertBefore(b,s)})();
   /* End of Include */
-</script>

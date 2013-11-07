df5671342a0d371f9dedf3ce3384b6e9c782c480
grantmiller <grant@look.io>
2013-11-07 08:04:59 -0800

    lp cdn tag

---
+++ js/lp-cdn-tag.js
@@ -0,0 +1,3 @@
+  /* LP Mobile JS include */
+  (function(){var a=_LP_CFG_.lpjsid="lpjs-"+(new Date).getTime(),b=document.createElement("script"),s=document.getElementsByTagName("script")[0];b.id=a;b.type="text/javascript";b.async=true;b.src="http://va-a.content.lpsnmedia.net/liveperson-mobile.js";s.parentNode.insertBefore(b,s)})();
+  /* End of Include */

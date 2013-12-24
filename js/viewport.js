3d56cf5b8b771d83d2f35e9c591be40ab8375272
grantmiller <grant@look.io>
2013-12-24 10:36:17 -0500

    Update viewport.js

--- js/viewport.js
+++ js/viewport.js
@@ -1,1 +1,1 @@
-document.write("<script type=\"text/javascript\" src=\"/tagtest/js/dynamic-viewport.js\"></script>");
+//document.write("<script type=\"text/javascript\" src=\"/tagtest/js/dynamic-viewport.js\"></script>");

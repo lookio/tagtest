70fc656fec797398bd2a6662adeaab8b33f5cc60
grantmiller <grant@look.io>
2014-02-25 10:36:36 -0800

    Create dev-tag.js

---
+++ js/dev-tag.js
@@ -0,0 +1,3 @@
+/* LP Mobile JS include */
+  (function(){var a=_LP_CFG_.lpjsid="lpjs-"+(new Date).getTime(),b=document.createElement("script"),s=document.getElementsByTagName("script")[0];b.id=a;b.type="text/javascript";b.async=true;b.src="https://s3.amazonaws.com/look-test-html-lib/lp_lib/liveperson-mobile.js";s.parentNode.insertBefore(b,s)})();
+/* End of Include */
